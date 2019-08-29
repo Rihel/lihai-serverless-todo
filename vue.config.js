faed9bbb41cfa60c07f0721e628a1b158b5182c1
Rihel <569843207@qq.com>
2019-08-29 11:11:56 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// const metaJson = require('./meta.json')
+const appJson = require('../../app.json')
+const outputDir = `${appJson.appKey}`
+const ossPath = `http://serverless-platform.deepexi.top/applications/${appJson.appKey}/`
+module.exports = {
+  // 打包资源路径应修改为oss上的东西
+  publicPath: process.env.NODE_ENV === 'production' ? ossPath : '/',
+  // 打包文件夹名  dist+物料hash
+  outputDir
+}
