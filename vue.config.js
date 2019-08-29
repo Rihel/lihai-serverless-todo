78cbc4f020803e6ab931305f8473b7cdc59adfaf
Rihel <569843207@qq.com>
2019-08-29 11:15:33 +0800

    配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // const metaJson = require('./meta.json')
-const appJson = require('../../app.json')
-const outputDir = `${appJson.appKey}`
-const ossPath = `http://serverless-platform.deepexi.top/applications/${appJson.appKey}/`
+const appJson = require('./app.json')
+const outputDir = `app${appJson.appKey}`
+const ossPath = `http://serverless-platform.deepexi.top/applications/${outputDir}`
 module.exports = {
   // 打包资源路径应修改为oss上的东西
   publicPath: process.env.NODE_ENV === 'production' ? ossPath : '/',
