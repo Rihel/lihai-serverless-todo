bf7ee76f1dddb91ddb862ae8c08273f8e9469d98
Rihel <569843207@qq.com>
2019-08-30 17:52:53 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-// const metaJson = require('./meta.json')
+require('dotenv').config()
 const appJson = require('./app.json')
+const env = process.env
 const outputDir = `app${appJson.appKey}`
 const ossPath = `http://serverless-platform.deepexi.top/applications/${outputDir}`
 module.exports = {
   // 打包资源路径应修改为oss上的东西
-  publicPath: process.env.NODE_ENV === 'production' ? ossPath : '/',
+  publicPath: env.NODE_ENV === 'production' ? ossPath : '/',
   // 打包文件夹名  dist+物料hash
   outputDir
 }
